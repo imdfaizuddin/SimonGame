09c3186c02270c411dbc1444b783cbcc4cd361b4
Md Faizuddin <imdfaizuddin@gmail.com>
2023-12-12 23:33:45 +0530

    added random box blinking and push value in array

--- app.js
+++ app.js
@@ -18,21 +18,55 @@ body.addEventListener("keydown" , function() {
 });
 
 function gameStart(params) {
-    let blue = document.querySelector(".blue");
-    let n = 1;
+    let n = randomNo();
     if(n===1){
-        blue.classList.remove("blue");
-        blue.style.border = 
-        setTimeout(() => {
-            blue.classList.add("blue");
-        }, 200);
+        // blue.classList.remove("blue");
+        
+        // setTimeout(() => {
+        //     blue.classList.add("blue");
+        // }, 200);
+        flash("blue");
+    }else if(n===2){
+        // red.classList.remove("red");
+        
+        // setTimeout(() => {
+        //     red.classList.add("red");
+        // }, 200);
+        flash("red");
+    }else if(n===3){
+        // yellow.classList.remove("yellow");
+        
+        // setTimeout(() => {
+        //     yellow.classList.add("yellow");
+        // }, 200);
+        flash("yellow");
+    }else if(n===4){
+        // green.classList.remove("green");
+        
+        // setTimeout(() => {
+        //     green.classList.add("green");
+        // }, 200);
+        flash("green");
     }
 }
+
+
+
+//helper functions
 
 function randomNo() {
     let r = Math.floor((Math.random()*4)+1);
     return r;
 }
 
+function flash(colorName) {
+    let boxName = document.querySelector(`.${colorName}`);
+    boxName.classList.remove(colorName);
+        
+        setTimeout(() => {
+            boxName.classList.add(colorName);
+        }, 200);
 
+    systemSeq.push(colorName);
+}
 
