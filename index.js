b8c6b6e16c912af6fc28dfa2deecc0409bb8b077
Md Faizuddin <imdfaizuddin@gmail.com>
2023-12-15 23:53:43 +0530

    added btn flash on user click

--- index.js
+++ index.js
@@ -17,7 +17,36 @@ document.addEventListener("keydown", function () {
     }
 });
 
+function btnFlash(btn){
+    btn.classList.add("bg--white");
+    setTimeout(() => {
+        btn.classList.remove("bg--white");
+    }, 300);
+}
+
 function levelUp(){
     level++;
     h1.innerText = `level ${level}`;
+
+    let randomIndex = Math.floor(Math.random()*3);
+    let randomColor = btns[randomIndex];
+    let randombtn = document.querySelector(`.${randomColor}`);
+    btnFlash(randombtn);
+}
+
+function btnFlash2(btn){
+    btn.classList.add("shadow--white");
+    setTimeout(() => {
+        btn.classList.remove("shadow--white");
+    }, 250);
+}
+function btnPress(){
+    // here this -- refers to the button pressed.
+   let btn = this;
+   btnFlash2(btn);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
 }
